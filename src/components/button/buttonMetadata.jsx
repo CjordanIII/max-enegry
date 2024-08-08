43840b2da474531e64428622a06d7d435ed93288
Clarence Jordan <clarencejordan087@gmail.com>
2024-08-08 11:06:15 -0500

    add btn

--- src/components/button/buttonMetadata.jsx
+++ src/components/button/buttonMetadata.jsx
@@ -1,13 +1,102 @@
 export const svg = true;
-
-export const lightBlueBtn = {
+export const noSvg = false;
+// light blue
+export const startToday = {
   style: `bg-sky-400 px-4 py-2 rounded-md text-white hover:bg-sky-300 active:bg-sky-950 ${
     svg && "flex justify-center gap-2 items-center"
   }`,
   btnLabel: "Start today",
 };
+export const handleStartTodayButtion = () => {
+  "use client";
+  console.log("working");
+};
 
-export const handleButtion = () => {
+export const contactUs = {
+  style: `bg-sky-400 px-4 py-2 rounded-md text-white hover:bg-sky-300 active:bg-sky-950 ${
+    svg && "flex justify-center gap-2 items-center"
+  }`,
+  btnLabel: "Contact Us",
+};
+export const handleContactUsButtion = () => {
+  "use client";
+  console.log("working");
+};
+
+export const reportOutage = {
+  style: `bg-sky-400 px-4 py-2 rounded-md text-white hover:bg-sky-300 active:bg-sky-950 ${
+    svg && "flex justify-center gap-2 items-center"
+  }`,
+  btnLabel: "Report Outage or Emergency",
+};
+export const handleReportOutageButtion = () => {
+  "use client";
+  console.log("working");
+};
+
+export const tryTaskeyFree = {
+  style: `bg-sky-400 px-4 py-2 rounded-md text-white hover:bg-sky-300 active:bg-sky-950 ${
+    svg && "flex justify-center gap-2 items-center"
+  }`,
+  btnLabel: "Try Taskey Free",
+};
+export const handleTryTaskeyFreeButtion = () => {
+  "use client";
+  console.log("working");
+};
+
+export const save = {
+  style: `bg-sky-400 px-4 py-2 rounded-md text-white hover:bg-sky-300 active:bg-sky-950 ${
+    svg && "flex justify-center gap-2 items-center"
+  }`,
+  btnLabel: "Save",
+};
+export const handleSaveButtion = () => {
+  "use client";
+  console.log("working");
+};
+
+export const send = {
+  style: `bg-sky-400 px-4 py-2 rounded-md text-white hover:bg-sky-300 active:bg-sky-950 ${
+    svg && "flex justify-center gap-2 items-center"
+  }`,
+  btnLabel: "Send",
+};
+export const handleSendButtion = () => {
+  "use client";
+  console.log("working");
+};
+
+export const payNow = {
+  style: `bg-sky-400 px-4 py-2 rounded-md text-white hover:bg-sky-300 active:bg-sky-950 ${
+    svg && "flex justify-center gap-2 items-center"
+  }`,
+  btnLabel: "Pay Now",
+};
+export const handlePayNowButtion = () => {
+  "use client";
+  console.log("working");
+};
+export const SignUp = {
+  style: `bg-sky-400 px-4 py-2 rounded-md text-white hover:bg-sky-300 active:bg-sky-950 ${
+    svg && "flex justify-center gap-2 items-center"
+  }`,
+  btnLabel: "Sign Up",
+};
+export const handleSignUpButtion = () => {
+  "use client";
+  console.log("working");
+};
+
+// dark blue
+
+export const signInRegister = {
+  style:
+    "bg-blue-950 px-4 py-2 rounded-md text-white hover:bg-blue-800 active:bg-slate-800",
+  btnLabel: "Sign in/Register",
+};
+
+export const handleSigInRegisterButtion = () => {
   "use client";
   console.log("working");
 };
