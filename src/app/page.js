67ed161be2c7d854e6c69108370475776b07d0e8
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-08-09 08:34:33 -0500

    add defaultTextBox

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,19 @@ import {
   svg,
 } from "../components/button/buttonMetadata.jsx";
 
+import "../styles/defaultTextBox.css";
+import DefaultTextBox from "../components/defaultTextBox/defaultTextBox.jsx";
+import {
+  lightGrayTextBox,
+  placeholderColor,
+} from "../components/defaultTextBox/defaultTextBoxMetadata.jsx";
+
 export default function Home() {
   const session = async () => {
     const session = await getServerSession();
     return session;
   };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <p>getServerSession Result</p>
@@ -26,6 +34,13 @@ export default function Home() {
           btnLabel={lightBlueBtn.btnLabel}
           svg={svg}
           handleButtion={handleButtion}
+        />
+      </div>
+      <div className="absolute top-15 right-40">
+        <DefaultTextBox
+          textBoxLabel={lightGrayTextBox.textBoxLabel}
+          style={lightGrayTextBox.style}
+          placeholderClass={placeholderColor.className}
         />
       </div>
     </main>
