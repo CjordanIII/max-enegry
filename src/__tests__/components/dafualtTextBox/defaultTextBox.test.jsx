9e54533dc6f1e09317068a065e84329d79eb1b9d
Clarence Jordan <clarencejordan087@gmail.com>
2024-08-13 11:28:44 -0500

    asdjfsla

--- src/__tests__/components/dafualtTextBox/defaultTextBox.test.jsx
+++ src/__tests__/components/dafualtTextBox/defaultTextBox.test.jsx
@@ -1,42 +1,42 @@
 import DefaultTextBox from "@/components/defaultTextBox/defaultTextBox.jsx";
 import "@testing-library/jest-dom";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 
 import {
-    lightGrayTextBox,
-    placeholderColor,
+  lightGrayTextBox,
+  placeholderColor,
 } from "../../../components/defaultTextBox/defaultTextBoxMetadata";
 
 describe("textbox test", () => {
-    it("renders", async () => {
-        render(
-            <DefaultTextBox
-                style={lightGrayTextBox.style}
-                textBoxLabel={lightGrayTextBox.textBoxLabel}
-                className={placeholderColor.className}
-            />
-        );
-        const input = screen.getByPlaceholderText(lightGrayTextBox.textBoxLabel);
-        await waitFor(() => expect(input).toBeInTheDocument());
-    });
-    
-    it("should have the same input and output value", () => {
-        const userValue = "Some Name";
+  it("renders", async () => {
+    render(
+      <DefaultTextBox
+        style={lightGrayTextBox.style}
+        textBoxLabel={lightGrayTextBox.textBoxLabel}
+        className={placeholderColor.className}
+      />
+    );
+    const input = screen.getByPlaceholderText(lightGrayTextBox.textBoxLabel);
+    await waitFor(() => expect(input).toBeInTheDocument());
+  });
 
-        render(
-            <DefaultTextBox
-              style={lightGrayTextBox.style}
-              textBoxLabel={lightGrayTextBox.textBoxLabel}
-              className={placeholderColor.className}
-            />
-          );
+  it("should have the same input and output value", () => {
+    const userValue = "Some Name";
 
-          const inputElement = screen.getByPlaceholderText(lightGrayTextBox.textBoxLabel);
+    render(
+      <DefaultTextBox
+        style={lightGrayTextBox.style}
+        textBoxLabel={lightGrayTextBox.textBoxLabel}
+        className={placeholderColor.className}
+      />
+    );
 
-          fireEvent.change(inputElement, { target: { value: userValue } });
-          
-          expect(inputElement.value).toBe(userValue);
-    });
+    const inputElement = screen.getByPlaceholderText(
+      lightGrayTextBox.textBoxLabel
+    );
+
+    fireEvent.change(inputElement, { target: { value: userValue } });
+
+    expect(inputElement.value).toBe(userValue);
+  });
 });
-
