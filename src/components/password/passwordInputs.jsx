87708bce2092fe33c5b6df107d7e39fa994bb13c
Daniel Hernandez <dh893177@gmail.com>
2024-08-14 11:49:46 -0500

    finished test

--- src/components/password/passwordInputs.jsx
+++ src/components/password/passwordInputs.jsx
@@ -1,15 +1,16 @@
 "use client";
 
-const PasswordInput = ({ label, inputClassName, labelClassName }) => {
+const PasswordInput = ({ label, inputClassName, labelClassName, inputId }) => {
     return (
         <div>
             {label && (
-                <label className={`${labelClassName} block text-sm`}>
+                <label htmlFor={inputId} className={`${labelClassName} block text-sm`}>
                     {label}
                 </label>
             )}
             <input
                 type="password"
+                id={inputId}
                 className={inputClassName}
                 required
             />
--- src/components/password/passwordInputsMetadata.jsx
+++ src/components/password/passwordInputsMetadata.jsx
@@ -2,10 +2,12 @@ export const lightGrayBox = {
     inputClassName: "bg-gray-200",
     label: "Password",
     labelClassName: "font-extrabold text-blue-500",
+    inputId:"password-input"
 };
 
 export const passwordConfirm = {
     inputClassName: "bg-gray-200",
     label: "Confirm Password",
-    labelClassName: "font-extrabold text-blue-500"
+    labelClassName: "font-extrabold text-blue-500",
+    inputId:"password-input"
 }
