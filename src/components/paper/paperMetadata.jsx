26d8105be889ce8300f85b45f6d00ad8c3629905
CjordanIII <118429283+CjordanIII@users.noreply.github.com>
2024-08-14 11:16:56 -0500

    Paper component test (#7)
    * add paper components
    
    * add papper
    
    ---------
    
    Co-authored-by: Wilfredo Contreras <wilfredojrcontreras@gmail.com>

---
+++ src/components/paper/paperMetadata.jsx
@@ -0,0 +1,9 @@
+export const babyBluePaper = {
+    style: 'bg-blue-200 p-5 rounded-lg shadow-lg w-[506px] h-[449px] flex items-center justify-center text-center font-bold',
+    paperLabel: "Baby Blue Example"
+}
+
+export const darkBluePaper = {
+    style: "bg-blue-900 p-5 rounded-lg shadow-lg w-[506px] h-[449px] flex items-center justify-center text-center text-white font-bold",
+    paperLabel:"Dark Blue Example"
+}
