9ed714539c2e4f1738f20a4329f8beb8900911ca
Daniel Hernandez <dh893177@gmail.com>
2024-08-23 12:27:16 -0500

    finished section 3 and 4

--- src/app/landing_page/page.jsx
+++ src/app/landing_page/page.jsx
@@ -1,10 +1,14 @@
 import SectionOne from "../../components/landingpage/SectionOne";
 import SectionTwo from "../../components/landingpage/SectionTwo";
+import SectionThree from "../../components/landingpage/SectionThree";
+import SectionFour from "../../components/landingpage/SectionFour";
 const LandingPage = () => {
   return (
     <div>
       <SectionOne />
       <SectionTwo />
+      <SectionThree />
+      <SectionFour />
     </div>
   );
 };
