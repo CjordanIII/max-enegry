dc5380268e63226e284d2c61a64bdf3d034b343f
CjordanIII <118429283+CjordanIII@users.noreply.github.com>
2024-09-04 11:24:47 -0500

    About us page (#19)
    * about us page done
    
    * safty push
    
    * test
    
    * test
    
    * add footer, delete thirdsection
    
    ---------
    
    Co-authored-by: Daniel Hernandez <dh893177@gmail.com>

---
+++ src/app/about_us_page/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import SectionTwo from "../../components/aboutUsPage/sectionTwo";
+import Footer from "../../components/footer/Footer";
+const AboutUsPage = () => {
+  return (
+    <div>
+      <SectionTwo />
+      <Footer />
+    </div>
+  );
+};
+
+export default AboutUsPage;
