acabed2fae996c253253fb70fce64e84a3c3224c
Daniel Hernandez <dh893177@gmail.com>
2024-09-04 11:03:15 -0500

    test

--- src/app/about_us_page/page.jsx
+++ src/app/about_us_page/page.jsx
@@ -1,3 +1,7 @@
+import sectionTwo from "../../components/aboutUsPage/sectionTwo";
+import sectionThree from "../../components/aboutUsPage/sectionThree";
+
+
 const AboutUsPage = () => {
   return (
     <div>
